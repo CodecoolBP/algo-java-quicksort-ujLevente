21f1e24450980fba663c2583841f96d7c45381c0
Uj Levente <asdlevente@gmail.com>
2019-02-17 16:45:54 +0100

    quicksort passes tests

--- src/main/java/com/codecool/quicksort/Main.java
+++ src/main/java/com/codecool/quicksort/Main.java
@@ -1,9 +1,15 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         // Use the tests to check your code!
+        QuickSort qs = new QuickSort();
+//        qs.sort(Arrays.asList(15, 3, 2, 1, 9, 5, 7, 8, 6));
+        qs.sort(Arrays.asList(2, 6, 5, 3, 8, 7, 1, 0));
+//        6, 3, 2, 1, 7, 5, 9, 8, 15
     }
 
 }
--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -1,5 +1,6 @@
 package com.codecool.quicksort;
 
+
 import java.util.List;
 
 public class QuickSort {
@@ -8,7 +9,65 @@ public class QuickSort {
      * Sorts the given List in place
      * @param toSort the List to sort. Throws an error if its null
      */
-    public void sort(List<Integer> toSort) {
 
+    public void sort(List<Integer> toSort) {
+        if (toSort == null)
+            throw new IllegalArgumentException("list not defined");
+
+        if (toSort.size() == 0)
+            return;
+
+        sort(toSort, 0, toSort.size() - 1);
     }
+
+    private void sort(List<Integer> toSort, int start, int end) {
+        if (start == end)
+            return;
+
+        int middleIndex = (start + end) / 2;
+        System.out.println(toSort);
+        System.out.println("============================");
+        System.out.println("start: " + start + " end: " + end + " middle index: " + middleIndex);
+
+        int pivotValue = toSort.get(middleIndex);
+
+        int left = start;
+        int right = end;
+        int partitionPoint;
+
+        while (true) {
+            while (toSort.get(left) < pivotValue)
+                left++;
+            while (toSort.get(right) > pivotValue)
+                right--;
+
+            if (left <= right) {
+                swapToElements(toSort, left, right);
+                left++;
+                right--;
+            } else {
+                partitionPoint = left;
+                break;
+            }
+        }
+
+        sort(toSort, start, partitionPoint - 1);
+        sort(toSort, partitionPoint, end);
+        System.out.println("============================");
+        System.out.println(toSort);
+    }
+
+    private void swapToElements(List<Integer> list, int leftIndex, int rightIndex) {
+        Integer leftElem = list.get(leftIndex);
+        Integer rightElem = list.get(rightIndex);
+        list.set(leftIndex, rightElem);
+        list.set(rightIndex, leftElem);
+    }
+
+//    private void moveElementToEnd(List<Integer> toSort, int index) {
+//        Integer value = toSort.get(index);
+//        toSort.remove(index);
+//        toSort.add(value);
+//    }
+
 }
